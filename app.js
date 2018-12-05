4db0b4df328c76ae010abb6d30aff3c43be3f537
Kha N. Do <donguyenkha@gmail.com>
2018-12-05 13:07:09 +0700

    Create account working

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const db = require('./lib/db');
+const Account = require('./lib/account');
 const server = require('./lib/server');
 
 db.sync().then(async () => {
   console.log('Database schema synced!');
+  // Try to init genesis account
+  const count = await Account.count();
+  if (count === 0) {
+    await Account.create({
+      address: process.env.GENESIS_ADDRESS,
+      balance: 0,
+      sequence: 0,
+      bandwidth: 0,
+    });
+  }
   const port = process.env.PORT || 26658;
   server.listen(port);
   console.log(`Server is listening on port ${port}`);
