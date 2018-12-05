f9c54f41e6c6c5b382ef0454c7be0b1835c3b105
Kha N. Do <donguyenkha@gmail.com>
2018-12-05 22:33:39 +0700

    Add mainnet genesis

--- generate-keypair.js
+++ generate-keypair.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
 const { Keypair } = require('stellar-base');
+const { createHash } = require('crypto')
 
 const key = Keypair.random();
-console.log(key.secret());
-console.log(key.publicKey());
+console.log('Secret key:', key.secret());
+console.log('Public key:', key.publicKey());
+
+console.log('Secret key (base64):', key._secretKey.toString('base64'));
+console.log('Public key (base64):', key._publicKey.toString('base64'));
+console.log('Tenermint address:', createHash('sha256')
+  .update(key._publicKey)
+  .digest().slice(0, 20)
+  .toString('hex')
+  .toUpperCase());
